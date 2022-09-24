be45eb87417b2ed29e3668f4e0dd6d5a845cdd93
Igor Fedenko <i.fedenko@module.ru>
2022-09-24 21:36:22 +0300

    add PMUStatuses class.

--- sdimabdk_testlib.py
+++ sdimabdk_testlib.py
@@ -21,7 +21,7 @@ class SDIMABDKError(Exception):
     ) -> None:
         self.error_code = error_code
         super().__init__(
-            message if error_code is None else f"{message} [code: {error_code}]"
+            message if error_code is None else f"[code: {error_code}] {message}"
         )
 
 
@@ -91,23 +91,23 @@ class VSCANId:
             sid: Optional[int] = None,
             s_fid: Optional[int] = None,
             p: Optional[int] = None,
-            l: Optional[int] = None,
-            s: Optional[int] = None,
+            local_bus: Optional[int] = None,
+            msg_type: Optional[int] = None,
             c_fid: Optional[int] = None,
             lcc: Optional[int] = None,
     ) -> None:
         """
         :param str rci:
-             идентификатор резервирования канала
+            идентификатор резервирования канала
         :param sid:
             идентификатор модуля сервера
         :param s_fid:
             идентификатор функции сервера
         :param p:
             приватность, всегда 1
-        :param l:
+        :param local_bus:
             локальная шина, всегда 1
-        :param s:
+        :param msg_type:
             тип сообщения, клиент/запрос - 1, сервер/ответ - 0
         :param c_fid:
             идентификатор функции клиента.
@@ -132,12 +132,8 @@ class VSCANId:
 
         # read information about the msgid from __MESSAGE_STRUCTURE class.
         for bit in self.MessageStructure:
-            id = self.message_id
             at, bit_count, test_val = bit.value
-            id.insert(at,
-                self.make_bits(bit_count, test_val if test_mode else params[at - 1])
-            )
-
+            self.message_id.insert(at, self.make_bits(bit_count, test_val if test_mode else params[at - 1]))
         self.message_id[8] = None
 
     def __repr__(self):
@@ -146,68 +142,97 @@ class VSCANId:
                         for bit in self.MessageStructure])
 
 
-#print(VSCANId())
+class Element:
+    """
+    An element class for PMUStatus table.
+    """
 
-
-class PSDone(Enum):
-    PS_LOADED: 1
-    PS_NOT_LOADED: 0
-
-
-class PSError(Enum):
-    PS_NO_ERROR: 0
-    PS_ERROR: 1
-
-class VIPStatus(Enum):
-    VIP_VALID: 0
-    VIP_FAILED: 1
-
-class PowerStatuses(Enum):
-    PG_VCC_3V3: 0
-    PG_VCC_GTX_1V8: 1
-    PG_VCC_1V8: 2
-    PG_VCC_1V2: 3
-    CORE_START: 4
-    PG_VTT_DDR: 5
-    PG_VDD_DDR_1V2: 6
-    PG_VCCPS_PLL: 7
-    PG_VMGTAVTT: 8
-    PG_VMGTAVCC: 9
-    PG_MGTRAVCC: 10
-    PG_VPP: 11
-    PG_VCCPSDDR_PLL: 12
-    PG_VCCIO_1V8: 13
-    PS_DONE: (14, PSDone)
-    PS_ERR_OUT: (15, PSError)
-    BAT_INACTIVE: 16
-    VIP_FAIL: (17, VIPStatus)
-
-# class PMUStatus(Enum):
-#     #   ИМЯ    РАЗМЕР, СМЕЩЕНИЕ
-#     PMU_VERSION: (0, 4)
-#     POWER_STATUS: (1, 4, PowerStatuses)
-
-from dataclasses import dataclass
-
-class StatusElement:
-    def __init__(self, offset, size):
+    def __init__(
+            self,
+            offset,
+            size,
+            fields: Optional[list[str, int]] = None
+    ):
+        __offsets = []
         self._offset = offset
-        self._size = size
+        if type(size) == list:
+            self._elements = []
+            for _, val in enumerate(size):
+                # _ - индекс, val[1] - поле, val[0] - размер области
+                self._elements.insert(_, val[1])
+                __offsets.insert(_, val[0])
+            self._elements = list(zip(__offsets, self._elements))
+        else:
+            self._elements = list(zip(range(size), [[None for _ in range(8)] for _ in range(size)]))
+        if fields is not None:
+            self._elements = [[None for _ in range(8)] for _ in range(size)]
+            for idx, el in enumerate(list(fields)):
+                # idx - index, el[0] - поле, el[1] - номер бита
+                if not type(el[1]) == list:
+                    a, b = divmod(int(el[1]), 8)
+                    # print(self._elements[a][1])
+                    self._elements[a][b] = el[0]
+                    __offsets.insert(int(el[1]), el[1])
+                else:
+                    # el[1] - range.
+                    for i in el[1]:
+                        a, b = divmod(int(i), 8)
+                        self._elements[a][b] = el[0]
+                        __offsets.insert(int(i), i)
+            self._elements = list(zip(__offsets, self._elements))
 
-    def set(self, elements: Optional[Any]):
-        self._elements = elements
+    def elements(self):
+        return self._elements
 
-@dataclass
-class PMUStatus(object):
-    Version: StatusElement(0, 4)
-    PowerStatus: StatusElement(1, 4).set(PowerStatuses)
 
-    def __post_init__(self):
-        try:
-            self.Version = int(self.Version)
-        except (ValueError, TypeError):
-            # could not convert age to an integer
-            self.Version = None
+class PMUStatuses:
+    Version = Element(0, 4)
+    Power = Element(1, 4, [
+        ('PG_VCC_3V3', 0),
+        ('PG_VCC_GTX_1V8', 1),
+        ('PG_VCC_1V8', 2),
+        ('PG_VCC_1V2', 3),
+        ('CORE_START', 4),
+        ('PG_VTT_DDR', 5),
+        ('PG_VDD_DDR_1V2', 6),
+        ('PG_VCCPS_PLL', 7),
+        ('PG_VMGTAVTT', 8),
+        ('PG_VMGTAVCC', 9),
+        ('PG_MGTRAVCC', 10),
+        ('PG_VPP', 11),
+        ('PG_VCCPSDDR_PLL', 12),
+        ('PG_VCCIO_1V8', 13),
+        ('PS_DONE', 14),
+        ('PS_ERR_OUT', 15),
+        ('BAT_INACTIVE', 16),
+        ('VIP_FAIL', 17),
+        ('RESERVED', list(range(19, 32)))
+    ])
 
-status = PMUStatus()
-print(status.Version)
+    TelemetricData = [
+        Element(2, [(2, 'E_ADC_27V_VOLT'), (2, 'E_ADC_27V_CUR')]),
+        Element(3, [(2, 'E_ADC_CAP_VOLT'), (2, 'TOTAL_POWER')]),
+        Element(4, [(2, 'ADC_5V_VOLT'), (2, 'ADC_5V_CUR')]),
+        Element(5, [(2, 'ADC_9V_VOLT'), (2, 'ADC_9V_CUR')]),
+        Element(6, [(2, 'MEAS_CORE_VOLT'), (2, 'MEAS_CORE_CUR')]),
+        Element(7, [(2, 'MEAS_3V3_VOLT'), (2, 'MEAS_3V3_CUR')]),
+        Element(8, [(2, 'MEAS_GTX_VOLT'), (2, 'MEAS_GTX_CUR')]),
+        Element(9, [(2, 'MEAS_DDR4_VOLT'), (2, 'MEAS_DDR4_CUR')]),
+        Element(10, [(2, 'TOTAL_MUEP_POWER'), (2, 'RESERVED')]),
+    ]
+
+    Temperature = [
+        Element(11, [(1, 'MEAS_VIP_TEMP'), (1, 'E_ADC_VIP_TEMP'), (1, 'E_ADC_LT8210_TEMP'), (1, 'E_ADC_LT8705_TEMP')]),
+        Element(12, [(1, 'MEAS_PMU_TEMP'), (1, 'MEAS_MUEP_TEMP'), (1, 'MEAS_MHPO_TEMP'), (1, 'TERMO_PMU_AVBL')]),
+        Element(13, [(1, 'MEAS_FPGA_TEMP'), (1, 'MEAS_LTM_1_TEMP'), (1, 'MEAS_LTM_2_TEMP'), (1, 'TERMO_MUEP_AVBL')]),
+    ]
+
+    CalibrationValues = [
+        Element(14, [(2, 'ADC_CALIBR'), (2, 'E_ADC_CALIBR_1')]),
+        Element(15, [(2, 'E_ADC_CALIBR_0'), (2, 'RESERVED'), (4, 'BATTERY_POWER_STATUS')]),
+    ]
+
+
+s = PMUStatuses
+
+print(s.Temperature[2].elements())
