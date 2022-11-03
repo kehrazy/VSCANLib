2622610a8368755c1cde49aab786ebcbad01ab91
Igor Fedenko <i.fedenko@module.ru>
2022-11-03 22:46:02 +0300

    vryawghrfahwfawf

--- sdimabdk_testlib.py
+++ sdimabdk_testlib.py
@@ -3,7 +3,7 @@ VSCAN Interface for SD IMA BK.
 """
 import ctypes
 import inspect
-from bifield import Bitfield
+
 
 import logging
 import string
@@ -13,7 +13,6 @@ from enum import Enum
 from typing import Any, Optional
 
 import vs_can_lib
-from bits import Bits
 from vs_can_lib import VSCANException
 
 
@@ -40,46 +39,40 @@ class SDIMABDKOperationError(SDIMABDKError):
     pass
 
 
-class Msg(ctypes.Structure):
-    _fields_ = [("rci", ctypes.c_int, 2),
-                ("sid", ctypes.c_int, 7),
-                ("sfid", ctypes.c_int, 7),
-                ("p", ctypes.c_int, 1),
-                ("l", ctypes.c_int, 1),
-                ("s", ctypes.c_int, 1),
-                ("client_fid", ctypes.c_int, 7),
-                ("lcc", ctypes.c_int, 3),
-                ("pad", ctypes.c_int, 3)]
 
 
-class MsgUnion(ctypes.Union):
-    _fields_ = [
-        ('bits', Msg),
-        ('id', ctypes.c_int),
-    ]
+
+
 
 
 class VSCANId:
-    
-    # class MessageStructure(Enum):
-    #     BUS_NUMBER = (0, 1, 1)
-    #     SERVER_ID = (2, 7, 1)
-    #     SERVER_FUNC_ID = (15, 1, 121)
-    #     PRIVACY = (16, 1, 1)
-    #     LOCAL_BUS = (17, 1, 1)
-    #     MSG_TYPE = (18, 1, 1)
-    #     CLIENT_FUNC_ID = (19, 7, 121)
-    #     LOGICAL_CHANNEL_NUM = (26, 2, 6)
-    #     UNK = (28, 3, 0)v
+    class MsgUnion(ctypes.Union):
+        class Msg(ctypes.Structure):
+            _fields_ = [("rci", ctypes.c_int, 2),
+                        ("sid", ctypes.c_int, 7),
+                        ("sfid", ctypes.c_int, 7),
+                        ("p", ctypes.c_int, 1),
+                        ("l", ctypes.c_int, 1),
+                        ("s", ctypes.c_int, 1),
+                        ("client_fid", ctypes.c_int, 7),
+                        ("lcc", ctypes.c_int, 3),
+                        ]
+        
+        _fields_ = [
+            ('bits', Msg),
+            ('id', ctypes.c_int),
+        ]
+
+        
     
     def __init__(
             self,
-            rci: Optional[int] = None,
+            rci = None,
             sid: Optional[int] = None,
             s_fid: Optional[int] = None,
             p: Optional[int] = None,
-            local_bus: Optional[int] = None,
-            msg_type: Optional[int] = None,
+            l: Optional[int] = None,
+            s: Optional[int] = None,
             c_fid: Optional[int] = None,
             lcc: Optional[int] = None,
     ) -> None:
@@ -91,81 +84,33 @@ class VSCANId:
         if not test_mode and None in params:
             raise SDIMABDKOperationError('Invalid parameters!')
         
-        self.message_id = 0x0
-        
-        # read information about the msgid from __MESSAGE_STRUCTURE class.
-        # test output should be 0x1bfff020 [11011111111111111000000100000]
-        # for bit in self.MessageStructure:
-        #     pos, count, val = bit.value
-        #     self.message_id |= val << pos
-        #     print(f'{hex(self.message_id)}: {bin(self.message_id)}')
-        # print(f'{bin(0x1BFFF020)[2:]}')
-        # self.message_id = (int(bin(self.message_id), 2) | int(bin((test_val << bit_pos)), 2))
-        # print(f'{"".join([str(x).ljust(1) for x in bin(self.message_id)[2:]])}')
-        # print(' ' * bit_pos + '^')
-        # print(' ' * bit_pos + f'{bin(test_val)[2:]}({test_val})@{bit_pos}')
-        # Todo: this
-        # test = MsgUnion()
-        # print(ctypes.sizeof(Msg))
-        # test.bits.rci = 5
-        # test.bits.sid = 8
-        # test.bits.sfid = 120
-        # test.bits.p = 1
-        # test.bits.l = 1
-        # test.bits.s = 1
-        # test.bits.cfid = 127
-        # test.bits.lcc = 6
-        # test.bits.pad = 0
-        # print(hex(test.id))
-        a = Bits(0xFFFFFFF)
-        a[0:1] = 1
-        a[1:8] = 8
-        a[8:15] = 120
-        a[16] = 1
-        a[17] = 1
-        a[18] = 1
-        a[18:25] = 127
-        a[25:28] = 6
-        print(hex(a))
-        # test = MsgUnion()
-        # test.bits.rci = 0
-        # test.bits.sid = 112
-        # test.bits.sfid = 127
-        # test.bits.p = 1
-        # test.bits.l = 1
-        # test.bits.s = 1
-        # test.bits.client_fid = 0
-        # test.bits.lcc = 0
-        # print(hex(test.id))
-        
-        # test = MsgUnion()
-        # test.id = 0x1bfff020
-        # print(test.bits.rci)
-        # print(test.bits.sid)
-        # print(test.bits.sfid)
-        # print(test.bits.p)
-        # print(test.bits.l)
-        # print(test.bits.s)
-        # print(test.bits.client_fid)
-        # print(test.bits.lcc)
-        
-        # print('-'*25 + '\n')
-        # print(f'{bin(self.message_id)[2:]}')
-        # self.message_id = ctypes.c_uint32(self.message_id).value
-    
-    def test_bit(self):
-        mask = 1 << 18
-        return self.message_id & mask
-    
-    def __str__(self):
-        return str(self.message_id)
+        msg = self.MsgUnion()
+        msg.bits.rci = rci if not test_mode else 0
+        msg.bits.sid = sid if not test_mode else 8
+        msg.bits.sfid = s_fid if not test_mode else 120
+        msg.bits.p = p if not test_mode else 1
+        msg.bits.l = l if not test_mode else 1
+        msg.bits.s = s if not test_mode else 1
+        msg.bits.client_fid = c_fid if not test_mode else 127
+        msg.bits.lcc = lcc if not test_mode else 6
+
+        self.id = msg.id
+        self.bits = msg.bits
 
 
 def test():
-    # print(VSCANMessage(VSCANMessage.Functions.READ,VSCANMessage.Parameters.WORD,0x000000).message)
-    msg = VSCANId().message_id
-    # print(f'my: {len(bin(msg)[2:])} {bin(msg)[2:]}')
-    # print(f'og: {(len(bin(0x1BFFF020)[2:]))} {bin(0x1BFFF020)[2:]}')
+    msg = VSCANId(
+        rci=0,
+        sid=63,
+        s_fid=120,
+        p=1,
+        l=1,
+        s=1,
+        c_fid=127,
+        lcc=6
+    )
+    print(hex(msg.id))
+    print(msg.bits.sid)
 
 
 test()
